a9aced6e86210a81416766a921e5db21f574ffa1
Bram Houben <410994@student.fontys.nl>
2019-05-06 15:44:01 +0200

    algo

--- AlgoritmeProject/Program.cs
+++ AlgoritmeProject/Program.cs
@@ -6,7 +6,30 @@ namespace AlgoritmeProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //connstring
+
+            // begin algoritme
+
+
+            // als 1e de lijst met gefixeerde laag naar hoog
+
+            // Alle taken optellen die gekozen zijn van laag naar hoog, 0 achteraan
+
+
+            // mensen met het aantal voorkeuren van laag naar hoog
+
+            //mensen van hoog aantal beschikbare uren naar laag
+
+            //Check of er nog open/ niet ingevulde taken in de lijst staan
+
+            // mensen beschikbaar? zoniet opnieuw beginnen
+
+            // 
+
+
+
+
+
         }
     }
 }
