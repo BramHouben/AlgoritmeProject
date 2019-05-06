1ae733e43c49b12ece7f266fec17f4e9ceea57b9
Bram Houben <410994@student.fontys.nl>
2019-05-06 15:26:12 +0200

    Add project files.

---
+++ AlgoritmeProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgoritmeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
