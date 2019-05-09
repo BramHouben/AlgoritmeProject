b691eb30c9cb9dd7a57effa38741f806c03e81a2
Bram Houben <410994@student.fontys.nl>
2019-05-09 11:40:14 +0200

    g

---
+++ AlgoritmeProject/Algoritme.cs
@@ -0,0 +1,12 @@
+﻿namespace AlgoritmeProject
+{
+    public class Algoritme
+    {
+        public int Docent_id { get; set; }
+
+        public int Voorkeur_id { get; set; }
+
+        public int Prioriteit { get; set; }
+        public int Bekwaamheid_id { get; internal set; }
+    }
+}
--- AlgoritmeProject/Program.cs
+++ AlgoritmeProject/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace AlgoritmeProject
@@ -7,10 +9,11 @@ namespace AlgoritmeProject
     {
         private static void Main(string[] args)
         {
+            List<Algoritme> users = new List<Algoritme>();
             SqlConnection sqlConnection = new SqlConnection("Data Source=mssql.fhict.local;User ID=dbi410994;Password=Test123!;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
             sqlConnection.Open();
             //connstring
-
+            
             // begin algoritme
 
             // als 1e de lijst met gefixeerde laag naar hoog
@@ -21,9 +24,36 @@ namespace AlgoritmeProject
                 Console.WriteLine("Gefixeerde mensen ingedeeld!");
             }
             // Alle taken optellen die gekozen zijn van laag naar hoog, 0 achteraan
-            select Taak, count(taak)Aantal from Bekwaamheid group by Taak having count(Taak) > 0 order by Aantal
-            // mensen met het aantal voorkeuren van laag naar hoog
-             select UserId, count(UserId)Gekozen from Bekwaamheid group by UserId having count(UserId) > 0 order by Gekozen
+
+            using (sqlConnection)
+            {
+                
+                using (var command = new SqlCommand("SelecteerAlleinzetten", sqlConnection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                })
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        //var reader = command.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            var item = new Algoritme();
+                            item.Docent_id = (int)reader["DocentID"];
+                            item.Prioriteit = (int)reader["Prioriteit"];
+                            item.Voorkeur_id = (int)reader["VoorkeurID"];
+                            item.Bekwaamheid_id = (int)reader["Bekwaamheid_id"];
+                            users.Add(item);
+                        }
+
+                    }
+                }
+            }
+
+
+            //    "select Taak, count(taak)Aantal from Bekwaamheid group by Taak having count(Taak) > 0 order by Aantal";
+            //// mensen met het aantal voorkeuren van laag naar hoog
+            //"select UserId, count(UserId)Gekozen from Bekwaamheid group by UserId having count(UserId) > 0 order by Gekozen";
             //mensen van hoog aantal beschikbare uren naar laag
 
             //Check of er nog open/ niet ingevulde taken in de lijst staan
